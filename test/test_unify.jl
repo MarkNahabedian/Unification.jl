71ce9c06e8b950289aafd211c8f8d1d48e581e4f
MarkNahabedian <naha@mit.edu>
2021-11-07 14:49:26 -0500

    Add some tests to improver test coverage.  All tests pass.

--- test/test_unify.jl
+++ test/test_unify.jl
@@ -26,6 +26,8 @@ end
 end
 
 @testset "Unify Fields" begin
+    struct Struct0 end
+    struct Struct0a end
     struct Struct1
         a
         b
@@ -34,13 +36,26 @@ end
         a
         b
     end
-    @test test_unify(Struct1(1, 2), Struct1(1, 2)) == true
+    @test test_unify(Struct1(1, Struct0()), Struct1(1, Struct0())) == true
     @test test_unify(Struct1(1, 2), Struct1(1, 3)) == false
     @test test_unify(Struct1(1, 2), Struct2(1, 2)) == false
     @test test_unify(Struct1(Struct1(4, 5), 2), Struct1(Struct1(4, 5), 2)) == true
     @test test_unify(Struct1(Struct1(4, 5), 2), Struct1(Struct1(4, 0), 2)) == false
     # Pair has fields:
-    @tewt test_unify(:a => 1, :a => 1) == true
-    @tewt test_unify(:a => 1, :a => 2) == false
-    @tewt test_unify(:b => 1, :a => 1) == false
+    @test test_unify(:a => 1, :a => 1) == true
+    @test test_unify(:a => 1, :a => 2) == false
+    @test test_unify(:b => 1, :a => 1) == false
 end
+
+@testset "unify ignore" begin
+    @test test_unify(1, V"") == true
+    @test test_unify(V"", 2) == true
+    struct Struct1
+        a
+        b
+    end
+    @test test_unify(Struct1(V"", 2), Struct1(1, 2)) == true
+    @test test_unify(Struct1(2, V""), Struct1(2, 1)) == true
+    @test test_unify(Struct0(), Struct0a()) == false
+end
+
